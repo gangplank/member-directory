8bf5ea50a3cf940a513ff214b9af7951d2d04b2d
David Monaghan <github@gangplankhq.com>
2016-04-19 23:19:45 -0700

    Create join table members <-> skills.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420052210) do
+ActiveRecord::Schema.define(version: 20160420061830) do
 
   create_table "availabilities", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "member_skills", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,6 +30,11 @@ ActiveRecord::Schema.define(version: 20160420052210) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "members_skills", id: false, force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "skill_id",  null: false
   end
 
   create_table "skills", force: :cascade do |t|
